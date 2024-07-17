e065d866875daecc4984b1fb29a4b9a51fb192b7
hong seong hwa <60565249+KF9pu@users.noreply.github.com>
2024-07-17 10:46:29 +0900

    chore : 페이지 enum 길이 같이 리턴하도록 수정

--- src/enums/page.enum.ts
+++ src/enums/page.enum.ts
@@ -1,9 +1,11 @@
-export enum Page {
+enum Page {
   "Profile" = 0,
   "World" = 1,
   "FoodMap" = 2,
   "?" = 3,
 }
 
-const PageEnum = { Page };
+const pageCnt = Object.keys(Page).filter((key) => isNaN(Number(key))).length;
+
+const PageEnum = { Page, pageCnt };
 export default PageEnum;
